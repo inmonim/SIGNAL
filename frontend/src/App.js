403cc9253365f9f0832dd4ca84625f48697c7239
tableMinPark <tablemin_park@daum.net>
2023-01-17 02:16:09 +0900

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() { 
+  return (
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+          </header>
+          <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+      </div>
+  )
+}
+
+export default App;
