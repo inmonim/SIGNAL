9919fb37c8f43c86831a2739302d53c2f6c354c4
tablemin_park <tablemin_park@daum.net>
2023-01-19 17:39:46 +0900

    Feature : Apply 기초 작업

--- back-end/src/main/java/com/ssafysignal/api/apply/entity/Apply.java
+++ back-end/src/main/java/com/ssafysignal/api/apply/entity/Apply.java
@@ -1,4 +1,25 @@
 package com.ssafysignal.api.apply.entity;
 
+import com.ssafysignal.api.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "apply")
 public class Apply {
+    @Id
+    private int apply_seq;
+
+    @ManyToOne
+    private User user;
+
+    private String content;
+
+
 }
