d0054ecd31ce64e07812dbfd938bed65239ed73a
김현진 <kjinabc@gmail.com>
2023-01-20 15:20:02 +0900

    Feature : Create posting.jsx

--- front-end/src/pages/posting/posting.jsx
+++ front-end/src/pages/posting/posting.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
+import { Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { changeName,changeAge } from "./store";
 
-function posting(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function Cart() {
+  const state = useSelector((state) => {
+    return state
+  })
+  // console.log(a);
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      {state.user.name} {state.user.age}
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.jang.map((ele, i) => (
+            <tr key={i}>
+              <td>{ele.id}</td>
+              <td>{ele.name}</td>
+              <td>{ele.count}</td>
+              <td>
+                <button
+                  onClick={() => {
+                    dispatch(changeName())
+                    dispatch(changeAge())
+                  }}
+                >
+                  +
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  )
 }
-
-export default posting;
