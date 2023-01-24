aa28e79c85a36e49522df28c6feeed8013bf3185
myonjin <kjinabc@gmail.com>
2023-01-25 01:29:31 +0900

    Feature : posting_select

--- front-end/src/pages/posting/posting.jsx
+++ front-end/src/pages/posting/posting.jsx
@@ -62,6 +62,12 @@ function Posting() {
             <SkillText>Vue</SkillText>
           </Skillbtn>
         </SkillSelectBox>
+        <FilterSelect>
+          <option value="">부산관역시</option>
+          <option value="">서울특별시</option>
+          <option value="">잉이잉</option>
+          <option value="">옹옹</option>
+        </FilterSelect>
         <PostList>
           <PostingCardItem className="post-card" />
           <PostingCardItem />
@@ -97,7 +103,6 @@ const Field = styled.div`
   display: flex;
   flex-direction: row;
   padding: 3px 29px;
-  justify-content: space-between;
   flex-wrap: wrap;
 `
 const Fieldtext = styled.div`
@@ -113,6 +118,7 @@ const Fieldtext = styled.div`
   @media (max-width: 590px) {
     font-size: 14px;
   }
+  margin-right: 2rem;
 `
 const SkillSelectBox = styled.div`
   display: flex;
@@ -151,5 +157,45 @@ const PostList = styled.div`
       color: white;
       transition: 0.5s;
     }
+  }
+`
+const FilterSelect = styled.select`
+  width: 100%;
+  max-width: 378px;
+  height: 42px;
+  padding: 0 14px;
+  border: 1px solid #d7e2eb;
+  border-radius: 4px;
+  box-sizing: border-box;
+  background-position: calc(100% - 0.8rem) 49%;
+  background-size: 0.625rem 0.3125rem;
+  background-color: #fbfbfd;
+  background-image: url(/images/common/toggle-black.png);
+  background-repeat: no-repeat;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 1.6;
+  color: #263747;
+  appearance: none;
+  transition: color 0.08s ease-in-out, background-color 0.08s ease-in-out, border-color 0.08s ease-in-out,
+    box-shadow 0.08s ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    border: 1px solid #3396f4;
+    box-shadow: inset 0 0 0 1px#3396f4;
+  }
+  &:focus {
+    border: 1px solid #3396f4;
+    box-shadow: inset 0 0 0 1px #3396f4;
+    background-color: #fff;
+    color: #495057;
+  }
+
+  @media (max-width: 767px) {
+    max-width: 100%;
+    height: 38px;
+    margin-bottom: 8px;
+    font-size: 14px;
   }
 `
