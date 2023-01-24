a5a8a29835a8e30eeebad8dda8ec4d32ab365d81
myonjin <kjinabc@gmail.com>
2023-01-24 19:35:38 +0900

    Feature : layout

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import MainPage from 'pages/MainPage'
-
+import App from 'App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <MainPage />
+    <App />
   </React.StrictMode>
 )
 
