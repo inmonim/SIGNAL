2faf5e0e0a2d7e658f08c7e111b9a8eef44baa46
황수빈 <marybin99@naver.com>
2023-01-25 17:18:51 +0900

    Feature : 쪽지 화면 구성

---
+++ front-end/src/components/TrashLetter.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+
+function TrashLetter() {
+  return (
+    <>
+      <Typography sx={{ textAlign: 'left', fontSize: '44px', fontWeight: 'bold', margin: '50px' }}>휴지통</Typography>
+    </>
+  )
+}
+
+export default TrashLetter
