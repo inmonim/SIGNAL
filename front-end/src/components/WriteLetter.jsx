d548b366d6957d90195dd7f41881426888a38080
황수빈 <marybin99@naver.com>
2023-01-26 17:18:48 +0900

    Feature : 쪽지 전송

--- front-end/src/components/WriteLetter.jsx
+++ front-end/src/components/WriteLetter.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, TextField } from '@mui/material'
 import Btn from '@mui/material/Button'
 import { styled } from '@mui/material/styles'
 import './WriteLetter.css'
+import AlertModal from 'components/AlertModal'
 
 const inputStyle = {
   backgroundColor: '#DDDBEC',
@@ -38,6 +39,46 @@ const SignalBtn = styled(Btn)(({ theme }) => ({
 
 function WriteLetter() {
   //   const form = useRef()
+  const [alertOpen, setAlertOpen] = useState(false)
+  const [inputs, setInputs] = useState({
+    nickname: '',
+    title: '',
+    content: '',
+    userSeq: 1,
+  })
+
+  const handleInput = (e) => {
+    const { name, value } = e.target
+    const nextInputs = { ...inputs, [name]: value }
+    setInputs(nextInputs)
+    console.log(nextInputs)
+  }
+
+  const handleSend = () => {
+    fetch('http://tableminpark.iptime.org:8080/letter', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(inputs),
+    })
+      .then((response) => {
+        if (response.ok === true) {
+          setAlertOpen(true)
+          return response.json()
+        } else {
+          alert('다시 시도')
+        }
+      })
+      .then((data) => {
+        console.log(`data: ${JSON.stringify(data)}`)
+      })
+  }
+
+  const handleToSend = ({ index }) => {
+    setAlertOpen(false)
+    // index = 1
+  }
   return (
     <div className="write-form">
       <Typography sx={{ textAlign: 'left', fontSize: '44px', fontWeight: 'bold' }}>쪽지 쓰기</Typography>
@@ -45,25 +86,19 @@ function WriteLetter() {
       <div className="send">
         <label>받는 사람</label>
         <br />
-        <TextField id="filled-multiline-flexible" sx={inputStyle} name="letter_send" />
+        <TextField id="filled-multiline-flexible" sx={inputStyle} name="nickname" onChange={handleInput} />
         <br />
         <label>제목</label>
         <br />
-        <TextField id="filled-multiline-flexible" sx={inputStyle} name="letter_subject" />
+        <TextField id="filled-multiline-flexible" sx={inputStyle} name="title" onChange={handleInput} />
         <br />
         <label>내용</label>
         <br />
-        <TextField
-          id="filled-multiline-flexible"
-          multiline
-          minRows={10}
-          rows={10}
-          sx={inputStyle}
-          name="letter_content"
-        />
+        <TextField id="filled-multiline-flexible" multiline sx={inputStyle} name="content" onChange={handleInput} />
       </div>
       {/* </form> */}
-      <SignalBtn>전송</SignalBtn>
+      <SignalBtn onClick={handleSend}>전송</SignalBtn>
+      <AlertModal open={alertOpen} onClick={handleToSend} msg="전송되었습니다."></AlertModal>
     </div>
   )
 }
