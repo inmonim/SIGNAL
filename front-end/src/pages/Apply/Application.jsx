9d8aa98e9aa08db8e2f2db602f8a3d83a9b12f91
nayu99 <nyj3230@gmail.com>
2023-01-27 16:47:47 +0900

    Feature : 지원서 조회 userFetch & applyFetch

--- front-end/src/pages/Apply/Application.jsx
+++ front-end/src/pages/Apply/Application.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import '../../assets/styles/application.css'
 import styled from 'styled-components'
 import { Button } from '@mui/material'
 import CancelIcon from '@mui/icons-material/Cancel'
+import axios from 'axios'
 // import { useDispatch } from 'react-redux'
 // import { fetchPostingDetail } from '../../store/redux'
 
@@ -19,8 +20,6 @@ const Title = styled.h1`
 //   dispatch(fetchPostingDetail())
 // }, [])
 
-// const [posting, setPosting] = useState([])
-
 // const setPostingDetail = () => {
 //   state.posting.then((e) => {
 //     setPosting(e)
@@ -29,6 +28,36 @@ const Title = styled.h1`
 // }
 
 const Application = () => {
+  const [apply, setApply] = useState([])
+  const [nickname, setNickname] = useState([])
+  const [phone, setPhone] = useState([])
+  const [email, setEmail] = useState([])
+
+  const applyFetch = async () => {
+    const res = await axios.get('http://tableminpark.iptime.org:8080/apply/1')
+    setApply(res.data.body)
+    console.log(apply.applyCode)
+  }
+
+  const userFetch = async () => {
+    try {
+      const res = await axios.get('http://tableminpark.iptime.org:8080/user/1')
+      setNickname(res.data.body.nickname)
+      setPhone(res.data.body.phone)
+      setEmail(res.data.body.email)
+      console.log(phone)
+      console.log(email)
+      console.log('userFetch response')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    applyFetch()
+    userFetch()
+  }, [])
+
   return (
     <div className="container">
       <div className="project-name-section">
@@ -41,48 +70,48 @@ const Application = () => {
         <div className="name-position-section">
           <div className="name-section">
             <div className="label">이름</div>
-            <div>김현진</div>
+            <div>{nickname}</div>
           </div>
           <div className="position-section">
             <div className="label">포지션</div>
-            <div>FrontEnd</div>
+            <div>{apply.applyCode}</div>
           </div>
         </div>
         <div className="phone-section">
           <div className="label">전화번호</div>
-          <div>010-0000-0000</div>
+          <div>{phone}</div>
         </div>
         <div className="email-section">
           <div className="label">이메일</div>
-          <div>signal@signal.com</div>
+          <div>{email}</div>
         </div>
         <div className="skill-section">
           <div className="label">사용기술</div>
-          <div>김현진</div>
+          <div>기술1</div>
         </div>
       </div>
       <div className="career-exp-section">
         <div className="career-section">
           <div className="career-label">
-            <div>경력</div>
+            <div className="label">경력</div>
             <hr />
           </div>
           <div>career0</div>
         </div>
         <div className="exp-section">
           <div className="exp-label">
-            <div>경험</div>
+            <div className="label">경험</div>
             <hr />
           </div>
           <div>exp0</div>
         </div>
       </div>
       <div>
-        <div>하고싶은 말</div>
+        <div className="label">하고싶은 말</div>
         <div>잘 부탁드립니다</div>
       </div>
       <div>
-        <div>공고 작성자가 궁금한 점</div>
+        <div className="label">공고 작성자가 궁금한 점</div>
         <div>
           <div>Q1.질문1</div>
           <div>answer</div>
