ef8d4ea696699937de1a71a1d2d32ce7051b4c4d
황수빈 <marybin99@naver.com>
2023-01-27 16:56:04 +0900

    Feature : 쪽지 상세 조회

--- front-end/src/components/ToLetter.jsx
+++ front-end/src/components/ToLetter.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import { Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import ToLetterList from './ToLetterList'
 
 function ToLetter() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + `/letter/from/${sessionStorage.getItem('userSeq')}?page=1&size=10`, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.body)
+        setData(res.body)
+      })
+  }, [])
+
   return (
     <>
-      <Typography sx={{ textAlign: 'left', fontSize: '44px', fontWeight: 'bold', margin: '50px' }}>
-        보낸쪽지함
-      </Typography>
+      <div style={{ textAlign: 'left' }}>
+        <div style={{ display: 'inline-block', fontSize: '44px', fontWeight: 'bold', marginBottom: '26px' }}>
+          보낸쪽지함
+        </div>
+        <ToLetterList data={data}></ToLetterList>
+      </div>
     </>
   )
 }
-
 export default ToLetter
