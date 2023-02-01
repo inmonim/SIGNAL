1d36800c5a0f2eab41ea534153a1bef30e953449
김현진 <kjinabc@gmail.com>
2023-02-01 17:51:42 +0900

    Feature : postingpost fin

--- front-end/src/pages/Posting/PostingDetail.jsx
+++ front-end/src/pages/Posting/PostingDetail.jsx
@@ -1,130 +1,166 @@
 import React, { useState, useEffect } from 'react'
-
 import '../../assets/styles/applyDetail.css'
 import '../../assets/styles/skill.css'
+import ApplyDelete from '../../components/Apply/ApplyDelete'
+import { Fielddata } from 'data/Fielddata'
+import { Button } from '@mui/material'
+import { Experimental_CssVarsProvider as CssVarsProvider, styled } from '@mui/material/styles'
+import ModeEditIcon from '@mui/icons-material/ModeEdit'
+import { Link, useParams } from 'react-router-dom'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
-import { getPositionName } from 'data/Positiondata'
+import Localdata from 'data/Localdata'
+// import { useLocation } from 'react-router'
 
-import skillImage from '../../assets/image/Skilltest/React.png'
+const ApplyModify = styled(Button)(({ theme }) => ({
+  backgroundColor: '#574B9F',
+  color: theme.vars.palette.common.white,
+  '&:hover': {
+    backgroundColor: theme.vars.palette.common.white,
+    color: '#574B9F',
+  },
+}))
 
-const Application = () => {
+function PostingDetail() {
   const { id } = useParams()
-  console.log(id)
-  const [apply, setApply] = useState([])
-  const [user, setUser] = useState([])
-  const [position, setPosition] = useState([])
+  // console.log(id)
+  // const location = useLocation()
+  // const userSeq = location.state.userSeq
+  // const applySeq = location.state.applySeq
 
-  const applyFetch = async () => {
+  const applySeq = 60
+  const postingSeq = id
+
+  const [posting, setPosting] = useState()
+
+  const postingGetFetch = async () => {
     try {
-      const res = await axios.get(process.env.REACT_APP_API_URL + `/apply/1`)
-      setApply(res.data.body)
-      setPosition(getPositionName(res.data.body.position.code))
+      const res = await axios.get(process.env.REACT_APP_API_URL + '/posting/' + postingSeq)
+      setPosting(res.data.body)
       console.log(res.data.body)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  const userFetch = async () => {
-    try {
-      const res = await axios.get(process.env.REACT_APP_API_URL + `/user/1`)
-      setUser(res.data.body)
+      // console.log('applyFetch', res.data.body)
     } catch (error) {
       console.log(error)
     }
   }
 
   useEffect(() => {
-    applyFetch()
-    userFetch()
+    postingGetFetch()
   }, [])
 
   return (
-    <div className="container">
-      <div className="project-name-section">
-        <div className="project-name-label">프로젝트 이름</div>
-        <div className="project-title">
-          <div className="title">싸피 프로젝트 모집</div>
-        </div>
-        <hr />
-      </div>
-      <div className="apply-section">
-        <div className="application-detail-section">
-          <div className="name-position-section">
-            <div className="name-section">
-              <div className="label">이름</div>
-              <div>{user.nickname}</div>
-            </div>
-            <div className="position-section">
-              <div className="label">포지션</div>
-              <div>{position}</div>
-            </div>
-          </div>
-          <div className="phone-section">
-            <div className="label">전화번호</div>
-            <div>{user.phone}</div>
-          </div>
-          <div className="email-section">
-            <div className="label">이메일</div>
-            <div>{user.email}</div>
-          </div>
-          <div className="skill-section">
-            <div className="label">사용기술</div>
-            <div className="skillList-section">
-              {apply.skillList &&
-                apply.skillList.map((skill, index) => (
-                  <div className="skill" key={index}>
-                    <img src={skillImage} alt="skillImage" className="skill-image" />
-                    <span>{skill.name}</span>
-                  </div>
-                ))}
-            </div>
-          </div>
-        </div>
-        <div className="career-exp-section">
-          <div className="career-section">
-            <div className="career-label">
-              <div className="label">경력</div>
-              <hr />
-            </div>
+    <CssVarsProvider>
+      <div className="apply-detail-container">
+        <div className="apply-detail-width-section">
+          <div className="apply-detail-project-name-section">
             <div>
-              {apply.careerList &&
-                apply.careerList.map((career, index) => (
-                  <div className="career" key={index}>
-                    {career}
-                  </div>
-                ))}
+              <div className="apply-detail-project-name-label">
+                마감기한 : {posting ? posting.postingEndDt.slice(0, 10) : null}
+              </div>
+              <div className="apply-detail-project-title">{posting ? posting.subject : null}</div>
+            </div>
+            <div className="apply-detail-cancle-section">
+              <Link to={'/applymodify'} state={{ applySeq }}>
+                <ApplyModify variant="contained" startIcon={<ModeEditIcon />}>
+                  지원 수정
+                </ApplyModify>
+              </Link>
+              <ApplyDelete open={open} applySeq={applySeq}></ApplyDelete>
+              <button
+                onClick={() => {
+                  console.log(posting)
+                }}
+              ></button>
             </div>
           </div>
-          <div className="exp-section">
-            <div className="exp-label">
-              <div className="label">경험</div>
-              <hr />
+          <hr className="apply-detail-hr" />
+          <div className="apply-detail-application-section">
+            <div className="apply-detail-name-position-section2">
+              <div className="apply-detail-name-section">
+                <div style={{ display: 'flex' }}>
+                  <div className="apply-detail-label">프로젝트 기간</div>
+                  <div className="apply-detail-text-value">{posting ? posting.term + ' 주' : null}</div>
+                </div>
+              </div>
+              <div className="apply-detail-position-section">
+                <div style={{ display: 'flex' }}>
+                  <div className="apply-detail-label">분야</div>
+                  <div className="apply-detail-text-value">{posting ? Fielddata[posting.fieldCode].name : null}</div>
+                </div>
+              </div>
             </div>
-            <div>
-              {apply.expList &&
-                apply.expList.map((exp, index) => (
-                  <div className="exp" key={index}>
-                    {exp}
+            <div className="apply-detail-name-position-section2">
+              <div className="apply-detail-name-section">
+                <div style={{ display: 'flex' }}>
+                  <div className="apply-detail-label">진행 유형</div>
+                  <div className="apply-detail-text-value">
+                    {posting ? (posting.isContact ? '대면' : '비대면') : null}
                   </div>
-                ))}
+                </div>
+              </div>
+              <div className="apply-detail-position-section">
+                <div style={{ display: 'flex' }}>
+                  <div className="apply-detail-label">진행 지역</div>
+                  <div className="apply-detail-text-value">{posting ? Localdata[posting.localCode].name : null}</div>
+                </div>
+              </div>
+            </div>
+            <div className="apply-detail-name-position-section2">
+              <div className="apply-detail-name-section">
+                <div style={{ display: 'flex' }}>
+                  <div className="apply-detail-label">난이도</div>
+                  <div className="apply-detail-text-value">{posting ? posting.level : null}</div>
+                </div>
+              </div>
+              <div className="apply-detail-position-section">
+                <div style={{ display: 'flex' }}>
+                  <div className="apply-detail-label">포지션 인원</div>
+                  <div className="apply-detail-text-value"></div>
+                </div>
+              </div>
+            </div>
+
+            <div className="apply-detail-skill-section">
+              <div style={{ minWidth: '12.5%', alignItems: 'center' }}>
+                <span className="apply-detail-skill-label">사용기술</span>
+              </div>
+              <div className="apply-detail-skillList-section"></div>
+            </div>
+            <div className="apply-detail-career-exp-section">
+              <div style={{ width: '50%' }}>
+                <div className="apply-detail-career-section">
+                  <div className="apply-detail-career-label">
+                    <div className="apply-detail-career-label">경력</div>
+                    <hr className="apply-detail-hr-small" />
+                  </div>
+                  <div style={{ display: 'flex' }}>
+                    <div style={{ width: '90%' }}></div>
+                  </div>
+                </div>
+              </div>
+              <div style={{ width: '50%' }}>
+                <div className="apply-detail-exp-section">
+                  <div className="apply-detail-exp-label">
+                    <div className="apply-detail-exp-label">경험</div>
+                    <hr className="apply-detail-hr-small" />
+                  </div>
+                  <div style={{ display: 'flex' }}>
+                    <div style={{ width: '90%' }}></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="apply-detail-content-section">
+              <div className="apply-detail-label">하고싶은 말</div>
+              <div className="apply-detail-content"></div>
+            </div>
+            <div className="apply-detail-question-answer-section">
+              <div className="apply-detail-label">공고 작성자가 궁금한 점</div>
+              <div style={{ margin: '10px 0px' }}></div>
             </div>
           </div>
-        </div>
-        <div className="content-section">
-          <div className="label">하고싶은 말</div>
-          <div className="content">{apply.content}</div>
-        </div>
-        <div className="question-answer-section">
-          <div className="label">공고 작성자가 궁금한 점</div>
-          <div></div>
         </div>
       </div>
-      {/* <div className="submit-section">
-        <ApplyDelete open={open}></ApplyDelete>
-      </div> */}
-    </div>
+    </CssVarsProvider>
   )
 }
-export default Application
+export default PostingDetail
