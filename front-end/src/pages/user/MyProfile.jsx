cb152c08d879ac7efe662e5ca548fd1021d159d5
subin <marybin99@naver.com>
2023-02-03 04:02:39 +0900

    Feature : 회원수정, 비밀번호 변경, 회원탈퇴 화면 구성 및 데이터 불러오기

--- front-end/src/pages/user/MyProfile.jsx
+++ front-end/src/pages/user/MyProfile.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SignalBtn from 'components/common/SignalBtn'
-import 'assets/styles/myprofile.css'
+import 'assets/styles/profile/myprofile.css'
 import ProfileImg from 'assets/image/profileimg2.jpeg'
 import heart from 'assets/image/heart.png'
+import UserModifyModal from 'components/user/UserModifyModal'
+import UserPwdModal from 'components/user/UserPwdModal'
+import AlertModal from 'components/AlertModal'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 function MyProfile() {
+  const [userModifyOpen, setUserModifyOpen] = useState(false)
+  const handleToModify = () => {
+    setUserModifyOpen(true)
+  }
+  const handleModifyClose = () => {
+    setUserModifyOpen(false)
+  }
+  const [userPwdOpen, setUserPwdOpen] = useState(false)
+  const handleToPwd = () => {
+    setUserPwdOpen(true)
+  }
+  const handlePwdClose = () => {
+    setUserPwdOpen(false)
+  }
+
+  const [alertOpen, setAlertOpen] = useState('')
+  const navigate = useNavigate()
+
+  const [isLogin, setIsLogin] = useState(false)
+  useEffect(() => {
+    if (sessionStorage.getItem('userSeq') !== null) {
+      setIsLogin(true)
+    }
+  }, [])
+  const handleToOut = () => {
+    setAlertOpen(true)
+  }
+  const handleToMain = () => {
+    const userSeq = sessionStorage.getItem('userSeq')
+    setAlertOpen(false)
+    try {
+      axios
+        .delete(process.env.REACT_APP_API_URL + '/user/' + userSeq)
+        .then((res) => {
+          sessionStorage.removeItem('accessToken')
+          sessionStorage.removeItem('refreshToken')
+          sessionStorage.removeItem('userEmail')
+          sessionStorage.removeItem('username')
+          sessionStorage.removeItem('userSeq')
+          console.log(res)
+          setIsLogin(false)
+          navigate('/', { state: { isLogin } })
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    } catch (err) {
+      console.log(err)
+    }
+  }
   return (
     <div className="my-container">
       <div className="my-main">
@@ -25,9 +80,11 @@ function MyProfile() {
               sigheight="28px"
               sigfontsize="15px"
               sigborderradius={24}
+              onClick={handleToModify}
             >
               회원정보 수정
             </SignalBtn>
+            <UserModifyModal open={userModifyOpen} onClose={handleModifyClose}></UserModifyModal>
             <SignalBtn
               className="my-user-btn-pwd"
               sigwidth="100px"
@@ -35,9 +92,11 @@ function MyProfile() {
               sigfontsize="15px"
               sigborderradius={24}
               sx={userModifyStyle}
+              onClick={handleToPwd}
             >
               비밀번호 변경
             </SignalBtn>
+            <UserPwdModal open={userPwdOpen} onClose={handlePwdClose}></UserPwdModal>
             <SignalBtn
               className="my-user-btn-delete"
               sigwidth="100px"
@@ -45,9 +104,11 @@ function MyProfile() {
               sigfontsize="15px"
               sigborderradius={24}
               sx={userDeleteStyle}
+              onClick={handleToOut}
             >
               회원 탈퇴
             </SignalBtn>
+            <AlertModal msg="탈퇴하시겠습니까?" open={alertOpen} onClick={handleToMain}></AlertModal>
           </div>
         </div>
         <div className="my-profile-container">
