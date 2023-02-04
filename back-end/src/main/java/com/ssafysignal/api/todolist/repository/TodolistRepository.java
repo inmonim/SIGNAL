015e0cafac558a37646910fd094bad647ec6bcef
inmonim <cat1083014@gmail.com>
2023-02-04 20:04:38 +0900

    Feature : ToDoList 구현 완료

--- back-end/src/main/java/com/ssafysignal/api/todolist/repository/TodolistRepository.java
+++ back-end/src/main/java/com/ssafysignal/api/todolist/repository/TodolistRepository.java
@@ -4,6 +4,12 @@ import com.ssafysignal.api.todolist.entity.Todolist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TodolistRepository extends JpaRepository<Todolist, Integer> {
+    List<Todolist> findAllByUserSeq(Integer userSeq);
+
+    Optional<Todolist> findByProjectToDoSeq(Integer toDoSeq);
 }
