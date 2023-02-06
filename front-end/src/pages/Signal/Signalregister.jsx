4db1bae08cd1264c0424b774211e033df3b48c68
김현진 <kjinabc@gmail.com>
2023-02-06 17:58:08 +0900

    Feature : signal

--- front-end/src/pages/Signal/Signalregister.jsx
+++ front-end/src/pages/Signal/Signalregister.jsx
@@ -4,7 +4,7 @@ import SignalBtn from 'components/common/SignalBtn'
 import { useNavigate } from 'react-router-dom'
 import AlertModal from 'components/AlertModal'
 import 'assets/styles/signal.css'
-// import api from 'api/Api'
+import api from 'api/Api'
 
 const inputStyle = {
   backgroundColor: '#DDDBEC',
@@ -21,15 +21,22 @@ const inputStyle = {
 }
 
 function Signalregister() {
+  const navigate = useNavigate()
+
   const [inputs, setInputs] = useState({
-    projectSeq: '',
+    projectSeq: '1',
     subject: '',
     pptfile: '',
     uccUrl: '',
     deployUrl: '',
     README: '',
-    userSeq: sessionStorage.getItem('userSeq'),
   })
+  const [file, setFile] = useState(null)
+  console.log(file)
+  const handleChangeFile = (event) => {
+    console.log(event.target.files)
+    setFile(event.target.files[0])
+  }
   const [alertOpen, setAlertOpen] = useState(false)
   const handleInput = (e) => {
     const { name, value } = e.target
@@ -37,17 +44,36 @@ function Signalregister() {
     setInputs(nextInputs)
     console.log(nextInputs)
   }
-  // const handlesignalRegist = () => {
-  //   api.post(process.env.REACT_APP_API_URL + '/board/qna', JSON.stringify(inputs)).then((res) => setAlertOpen(true)
-  // }
   const handlesignalRegist = () => {
-    setAlertOpen(true)
+    const formData = new FormData()
+    formData.append('projectSeq', Number(inputs.projectSeq))
+    formData.append('subject', inputs.subject)
+    formData.append('uccUrl', inputs.uccUrl)
+    formData.append('deployUrl', inputs.deployUrl)
+    formData.append('pptfile', file)
+    formData.append('README', inputs.README)
+    console.log(formData)
+    console.log(JSON.stringify(formData))
+    api
+      .post(process.env.REACT_APP_API_URL + '/signalweek', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then((res) => {
+        console.log(res)
+        setAlertOpen(true)
+      })
+      .catch((res) => {
+        console.log(res)
+        console.log(formData)
+      })
   }
-  const navigate = useNavigate()
   const handleAlert = (e) => {
     setAlertOpen(false)
     navigate(`/signal`)
   }
+
   return (
     <div className="signal-page-container">
       <div className="signal-regist-container">
@@ -69,7 +95,7 @@ function Signalregister() {
           </div>
           <div className="signal-regist-title">
             <label>PPT파일</label>
-            <TextField id="filled-multiline-flexible" name="pptfile" multiline sx={inputStyle} onChange={handleInput} />
+            <input onChange={handleChangeFile} name="pptUrl" type="file" id="file" multiple="multiple"></input>
           </div>
           <div className="signal-regist-title">
             <label>UCC(YouTube 주소)</label>
