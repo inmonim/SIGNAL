fd78cbae1188aab6485127a7674dba8cfb51e1e7
subin <marybin99@naver.com>
2023-02-09 07:07:34 +0900

    Design : 404

---
+++ front-end/src/pages/fourzerofour.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function fourzerofour() {
+  return (
+    <div style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
+      <div style={{ height: '0px', paddingBottom: '56.25%', position: 'relative', width: '100%' }}>
+        <iframe
+          allowFullScreen=""
+          height="100%"
+          src="https://giphy.com/embed/vboZVH1oDiLdctj4V3/video"
+          style={{ left: '0px', position: 'absolute', top: '0px' }}
+          width="100%"
+        ></iframe>
+      </div>
+    </div>
+  )
+}
+
+export default fourzerofour
