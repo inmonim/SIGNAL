5d81e3ea863a3ec5f7872293d73a55180bd74ef2
inmonim <cat1083014@gmail.com>
2023-02-08 16:58:35 +0900

    Feature : 유저 하트, 프로젝트 유저 하트 조회 api 생성

--- back-end/src/main/java/com/ssafysignal/api/project/repository/ProjectUserHeartLogRepository.java
+++ back-end/src/main/java/com/ssafysignal/api/project/repository/ProjectUserHeartLogRepository.java
@@ -4,7 +4,11 @@ import com.ssafysignal.api.project.entity.ProjectUserHeartLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface ProjectUserHeartLogRepository extends JpaRepository<ProjectUserHeartLog, Integer> {
+
+    List<ProjectUserHeartLog> findAllByProjectUserSeq(Integer projectUserSeq);
 }
