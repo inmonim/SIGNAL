678cbf12f6552e9cc2685256d7609e9e8013e4cb
김현진 <kjinabc@gmail.com>
2023-02-10 11:12:45 +0900

    Feature : pdf

--- front-end/src/pages/Signal/SignalDetail.jsx
+++ front-end/src/pages/Signal/SignalDetail.jsx
@@ -7,6 +7,7 @@ import { useLocation } from 'react-router-dom'
 import 'assets/styles/signaldetail.css'
 import api from 'api/Api.js'
 import { Document, Page } from 'react-pdf'
+import SignalBtn from 'components/common/SignalBtn'
 
 function signalDetail() {
   const location = useLocation()
@@ -14,9 +15,7 @@ function signalDetail() {
   console.log(signalSeq)
   const [numPages, setNumPages] = useState(null)
   const [pageNumber, setPageNumber] = useState(1)
-  const [pdfDocument] = useState(
-    process.env.REACT_APP_API_URL + '/static/ppt/c478da35-e08b-4335-b077-3426484ca8c9.pptx'
-  )
+  // const [pdfDocument] = useState()
 
   function onDocumentLoadSuccess({ numPages }) {
     setNumPages(numPages)
@@ -107,16 +106,18 @@ function signalDetail() {
             {data.deployUrl}
           </div>
         </div>
-        <Document file={pdfDocument} onLoadSuccess={onDocumentLoadSuccess}>
-          <Page pageNumber={pageNumber} />
-        </Document>
+        <div style={{ width: '1280px  ', height: '720px', overflow: 'hidden' }}>
+          <Document file={process.env.REACT_APP_API_URL + data.pptUrl} onLoadSuccess={onDocumentLoadSuccess}>
+            <Page width={1280} height={720} pageNumber={pageNumber} />
+          </Document>
+        </div>
         <p>
-          <span onClick={() => (pageNumber > 1 ? setPageNumber(pageNumber - 1) : null)}>&lt;</span>
+          <SignalBtn onClick={() => (pageNumber > 1 ? setPageNumber(pageNumber - 1) : null)}>&lt;</SignalBtn>
           <span>
             Page {pageNumber} of {numPages}
           </span>
 
-          <span onClick={() => (pageNumber < numPages ? setPageNumber(pageNumber + 1) : null)}>&gt;</span>
+          <SignalBtn onClick={() => (pageNumber < numPages ? setPageNumber(pageNumber + 1) : null)}>&gt;</SignalBtn>
         </p>
         <div className="apply-detail-content-section">
           <label>README</label>
