3ffc2911300de590771205ed8b5596d422e1e932
subin <marybin99@naver.com>
2023-02-13 07:10:09 +0900

    Fix : Header

--- front-end/src/components/Layout/Header.jsx
+++ front-end/src/components/Layout/Header.jsx
@@ -153,7 +153,7 @@ function Header() {
                       <D.LinkWrapper href="/signal">프로젝트 보기</D.LinkWrapper>
                     </D.Li>
                     <D.Li>
-                      <D.LinkWrapper href="/signal/rank">명예의 전당</D.LinkWrapper>
+                      <D.LinkWrapper href="/signal/rankMain">명예의 전당</D.LinkWrapper>
                     </D.Li>
                   </D.Ul>
                 </D.Down>
