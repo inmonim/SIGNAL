5f92b250b90601d19cfa704883a6a5b85ab3e350
nayu99 <nyj3230@naver.com>
2023-02-14 22:03:32 +0900

    Feature : 로그인 후 다시 로드 & 큐앤에이 regist, modify 로그인 인증

--- front-end/src/components/Auth/PrivateRoute.jsx
+++ front-end/src/components/Auth/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom'
 
 const PrivateRoute = ({ loginAuthenticated, component: Component }) => {
   //   return loginAuthenticated ? Component : <Navigate to="/" {...alert('로그인이 필요합니다.')}></Navigate>
-  return loginAuthenticated ? Component : <Navigate to="/error"></Navigate>
+  return loginAuthenticated ? Component : <Navigate to="/"></Navigate>
 }
 
 export default PrivateRoute
