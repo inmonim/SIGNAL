021169001d7e7744dace2351df37efccc2a9b4aa
tableMinPark <tablemin_park@daum.net>
2023-02-16 22:13:03 +0900

    Fix : 시연용 시그널 위크 스케줄러 생성

--- back-end/src/main/java/com/ssafysignal/api/auth/controller/AuthController.java
+++ back-end/src/main/java/com/ssafysignal/api/auth/controller/AuthController.java
@@ -47,10 +47,8 @@ import java.util.Map;
             LoginResponse loginResponse = authService.login(email, password);
             return ResponseEntity.ok().body(BasicResponse.Body(ResponseCode.SUCCESS, loginResponse));
         } catch (NotFoundException e) {
-            e.printStackTrace();
             return ResponseEntity.badRequest().body(BasicResponse.Body(e.getErrorCode(), null));
         } catch (RuntimeException e) {
-            e.printStackTrace();
             return ResponseEntity.badRequest().body(BasicResponse.Body(ResponseCode.UNAUTHORIZED, null));
         }
     }
@@ -65,13 +63,10 @@ import java.util.Map;
             LoginResponse loginResponse = authService.reissue(refreshToken);
             return ResponseEntity.ok().body(BasicResponse.Body(ResponseCode.SUCCESS, loginResponse));
         } catch (UnAuthException e) {
-            e.printStackTrace();
             return ResponseEntity.badRequest().body(BasicResponse.Body(e.getErrorCode(), null));
         } catch (NotFoundException e) {
-            e.printStackTrace();
             return ResponseEntity.badRequest().body(BasicResponse.Body(e.getErrorCode(), null));
         } catch (RuntimeException e) {
-            e.printStackTrace();
             return ResponseEntity.badRequest().body(BasicResponse.Body(ResponseCode.UNAUTHORIZED, null));
         }
     }
@@ -87,13 +82,10 @@ import java.util.Map;
             authService.logout(TokenInfo.of(accessToken, refreshToken), email);
             return ResponseEntity.ok().body(BasicResponse.Body(ResponseCode.SUCCESS, true));
         } catch (UnAuthException e) {
-            e.printStackTrace();
             return ResponseEntity.badRequest().body(BasicResponse.Body(e.getErrorCode(), null));
         } catch (NotFoundException e) {
-            e.printStackTrace();
             return ResponseEntity.badRequest().body(BasicResponse.Body(e.getErrorCode(), false));
         } catch (RuntimeException e) {
-            e.printStackTrace();
             return ResponseEntity.badRequest().body(BasicResponse.Body(ResponseCode.UNAUTHORIZED, null));
         }
     }
