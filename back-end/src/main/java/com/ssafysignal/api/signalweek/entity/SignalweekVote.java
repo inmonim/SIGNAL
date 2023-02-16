5989c83e7d23a76867ea0fb4adef9d73a221a579
kwonhyeokgeun <xkhg0611x@naver.com>
2023-02-16 16:41:49 +0900

    Feature : 머지 실수 수정

--- back-end/src/main/java/com/ssafysignal/api/signalweek/entity/SignalweekVote.java
+++ back-end/src/main/java/com/ssafysignal/api/signalweek/entity/SignalweekVote.java
@@ -1,10 +1,12 @@
 package com.ssafysignal.api.signalweek.entity;
 
+
 import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,23 +16,22 @@ import javax.persistence.*;
 @DynamicUpdate
 @Builder
 @AllArgsConstructor
-@Table(name = "signalweek_rank")
-public class SignalweekRank {
+@Table(name = "signalweek_vote")
+public class SignalweekVote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "signalweek_rank_seq")
-    private Integer signalweekRankSeq;
-
-    @Column(name = "signalweek_schedule_seq")
-    private Integer signalweekScheduleSeq;
-
-    @Column(name = "ranking")
-    private Integer ranking;
+    @Column(name = "signalweek_vote_seq")
+    private Integer signalweekVoteSeq;
 
     @Column(name = "signalweek_seq")
     private Integer signalweekSeq;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "signalweek_seq", insertable = false, updatable = false)
-    private Signalweek signalweek;
+    @Column(name = "from_user_seq")
+    private Integer fromUserSeq;
+
+    @Column(name = "reg_dt")
+    private LocalDateTime regDt;
+
+    @Column(name = "signalweek_schedule_seq")
+    private Integer signalweekScheduleSeq;
 }
